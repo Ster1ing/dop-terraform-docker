f4ccbb9ab51fb655866149d66380354a7017ec86
otarakanov <otarakanov@oroinc.com>
2023-10-18 21:19:19 +0300

    - attempt 1

--- main.tf
+++ main.tf
@@ -1,1 +1,47 @@
-# main.tf
+terraform {
+    required_providers {
+        docker = {
+            source  = "kreuzwerker/docker"
+            version = "~> 2.23.0"
+        }
+    }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+    name = "nginx-img"
+    keep_locally = false
+    build {
+        path  = "./"        
+    }
+}
+
+resource "docker_container" "nginx" {
+    name  = "nginx-cnt"
+    image = docker_image.nginx.image_id
+    ports {
+        internal = 80
+        external = 8080
+    }
+}
+
+variable "db_root_password" {
+    type = string
+}
+
+resource "docker_container" "db" {
+    name  = "db"
+    image = "mariadb:latest"
+    restart = "always"    
+    env = [
+        "MYSQL_ROOT_PASSWORD=${var.db_root_password}",
+        "MYSQL_PASSWORD=password",
+        "MYSQL_USER=user",
+        "MYSQL_DATABASE=database"
+    ]
+    ports {
+        internal = 3306
+        external = 3306
+    }  
+}
